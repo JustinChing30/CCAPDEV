910feb33a669a79a6f731f689457539a83901f1a
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-02-10 12:16:28 +0800

    reply button

--- Posts/script.js
+++ Posts/script.js
@@ -1,17 +1,17 @@
-const newPostBtn = document.getElementById("newPost");
-const newPostOverlay = document.getElementById("NewPostOverlay");
-const closeBtn = document.getElementById("close");
+const newPostBtn = document.getElementById("newPost"); // Button to open new post overlay
+const newPostOverlay = document.getElementById("NewPostOverlay"); // Button to close new post overlay
+
 const newposttextarea = document.getElementById("new-post-text");
+
+const replyButtons = document.querySelectorAll(".reply-button");
+const replyOverlay = document.getElementById("ReplyOverlay");
+const newreplytextarea = document.getElementById("new-reply-text");
+
+const closeButtons = document.querySelectorAll(".close-button");
 
 // Open new post overlay
 newPostBtn.addEventListener("click", () => {
   newPostOverlay.style.display = "flex";
-  newposttextarea.value = '';
-});
-
-// Close new post overlay
-closeBtn.addEventListener("click", () => {
-  newPostOverlay.style.display = "none";
   newposttextarea.value = '';
 });
 
@@ -21,4 +21,31 @@ newPostOverlay.addEventListener("click", (e) => {
     newPostOverlay.style.display = "none";
     newposttextarea.value = '';
   }
+});
+
+// Add listener to every single reply button
+replyButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    replyOverlay.style.display = "flex";
+    newposttextarea.value = '';
+  });
+});
+
+// Close reply overlay by clicking outside of the box
+replyOverlay.addEventListener("click", (e) => {
+  if (e.target === replyOverlay) {
+    replyOverlay.style.display = "none";
+    newreplytextarea.value = '';
+  }
+});
+
+// Add listener to every both post and reply overlay close button so they do the same thing
+closeButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    document.getElementById("NewPostOverlay").style.display = "none";
+    document.getElementById("ReplyOverlay").style.display = "none";
+    
+    document.getElementById("new-post-text").value = '';
+    document.getElementById("new-reply-text").value = '';
+  });
 });
