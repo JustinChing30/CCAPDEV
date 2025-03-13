7d4f13f1215aa140b50b4b6eb4d61d167299543e
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-03-14 01:54:58 +0800

    added topright to buncha stuff. missing it in ViewProfile1 and 2. also missingin the noedit vers

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,5 @@
-const newPostBtn = document.getElementById("newPost"); // Button to open new post overlay
-const newPostOverlay = document.getElementById("NewPostOverlay"); // Button to close new post overlay
 const profileButton = document.getElementById("profileDropdownBtn"); // Button to open dropdown
 const profileDropdown = document.getElementById("profileDropdown"); // The actual dropdown
-
-const newposttextarea = document.getElementById("new-post-text");
-const newposttitlearea = document.getElementById("new-post-title");
 
 const replyButtons = document.querySelectorAll(".reply-button");
 const replyOverlay = document.getElementById("ReplyOverlay");
@@ -12,28 +7,10 @@ const newreplytextarea = document.getElementById("new-reply-text");
 
 const closeButtons = document.querySelectorAll(".close-button");
 
-// Open new post overlay
-newPostBtn.addEventListener("click", () => {
-  newPostOverlay.style.display = "flex";
-  newposttextarea.value = '';
-  newposttitlearea.value = '';
-});
-
-// Close new post overlay by clicking outside of the box
-newPostOverlay.addEventListener("click", (e) => {
-  if (e.target === newPostOverlay) {
-    newPostOverlay.style.display = "none";
-    newposttextarea.value = '';
-    newposttitlearea.value = '';
-  }
-});
-
 // Add listener to every single reply button
 replyButtons.forEach((button) => {
   button.addEventListener("click", () => {
     replyOverlay.style.display = "flex";
-    newposttextarea.value = '';
-    newposttitlearea.value = '';
     newreplytextarea.value = '';
   });
 });
@@ -43,18 +20,14 @@ replyOverlay.addEventListener("click", (e) => {
   if (e.target === replyOverlay) {
     replyOverlay.style.display = "none";
     newreplytextarea.value = '';
-    newposttitlearea.value = '';
   }
 });
 
 // Add listener to every both post and reply overlay close button so they do the same thing
 closeButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    document.getElementById("NewPostOverlay").style.display = "none";
     document.getElementById("ReplyOverlay").style.display = "none";
     
-    document.getElementById("new-post-text").value = '';
-    document.getElementById("new-post-title").value = '';
     document.getElementById("new-reply-text").value = '';
   });
 });
